a0b3434fff6673cd8966945dd2c7e78401da9117
sicArchitect <maciej.kusztal1995@gmail.com>
2023-05-06 22:44:12 +0300

    Add task

---
+++ script.ts
@@ -0,0 +1,28 @@
+function showMyName(name: string): void {
+  console.log(name);
+}
+
+showMyName("John");
+
+function square(a: number): number {
+  return a * a;
+}
+
+square(10);
+
+function sum(title: string, ...numbers: number[]): string | number {
+  return title + " = " + numbers.reduce((sum, num) => sum + num, 0);
+}
+
+sum("Numbers", 1, 6, 10);
+
+const executeFunc = (
+  func: (title: string, num: number, arr: string[]) => void
+): void => {
+  const title = "Lorem Ipsum";
+  func(title, 24, ["Sport", "Movies"]);
+};
+
+executeFunc((title, age, hobbies) => {
+  return true;
+});
